769b8e4cb776e79e49e7a4c40ed4c28bf8939c94
Michelle Ho <chiachi_ho@g.harvard.edu>
2017-10-14 17:21:33 -0400

    added arrhenius and mod arrhenius coef class and associated tests

--- reaction_coefs.py
+++ reaction_coefs.py
@@ -3,32 +3,117 @@ import doctest
 
 class RxnCoef():
 	"""Base class for reaction coefficients"""
-	def __init__(self):
-		self.coef = None
 
-	def compute_coef(self):
+	def __init__(self):
+		self.k = None
+
+
+	def get_coef(self):
 		"""Not-implemented method to return the rate coefficient at base class"""
 		raise NotImplementedError('Subclass must implement this method')
 
 
 class ConstCoef(RxnCoef):
-	"""Subclass for constant reaction coefficients"""
+	"""Subclass of RxnCoef for constant reaction coefficients"""
 	def __init__(self, k):
-		self.coef = k
+		self.k = k
 
-	def compute_coef(self):
+	def get_coef(self):
 		"""Simply returns the constant rate coefficient
 
 		RETURNS:
 		========
-		coef: float
+		k: float
 			Constant reaction rate coefficient
 		
 		EXAMPLES:
 		=========
-		>>> test_coef_const.compute_coef()
+		>>> ConstCoef(10.0).get_coef()
 		10.0
 		"""
-		return self.coef
+		if self.k < 0:
+			raise ValueError("Negative reaction rate coefficients are prohibited.")
+		return self.k
 
-#doctest.testmod(extraglobs={'test_coef_const': ConstCoef(10.0)}, verbose=True)
+class ArrheniusCoef(RxnCoef):
+	"""Subclass of RxnCeof for Arrhenius reaction coefficients """
+	def __init__(self, A, E, T, R=8.314):
+		self.A = A
+		self.E = E
+		self.T = T
+		self.R = R
+		super().__init__()
+
+
+	def get_coef(self):
+		""" Method to calculate the Arrhenius reaction rate coefficient
+
+		RETURNS:
+		========
+		k: float
+			Arrhenius reaction rate coefficient
+
+		EXAMPLES:
+		=========
+		>>> ArrheniusCoef(2.0, 3.0, 100.0).get_coef()
+		1.9927962618542914
+		"""
+		if self.A < 0.0:
+			raise ValueError("A = {0:18.16e}:  Negative Arrhenius prefactor is prohibited!".format(self.A))
+
+		if self.T < 0.0:
+			raise ValueError("T = {0:18.16e}:  Negative temperatures are prohibited!".format(self.T))
+
+		if self.R < 0.0:
+			raise ValueError("R = {0:18.16e}:  Negative ideal gas constant is prohibited!".format(self.R))
+
+		else:
+			try:
+				self.k = self.A * np.exp(-self.E / (self.R * self.T))
+			except OverflowError:
+				raise OverflowError("The result is too large/small.")
+			return self.k
+
+
+class ModArrheniusCoef(ArrheniusCoef):
+	def __init__(self, A, b, E, T, R=8.314):
+		super().__init__(A)
+		super().__init__(E)
+		super().__init__(T)
+		super().__init__(R)
+		self.b = b
+
+	def get_coef(self):
+		""" Method to calculate the Modified Arrhenius reaction rate coefficient
+
+		RETURNS:
+		========
+		k: float
+			Modified Arrhenius reaction rate coefficient
+
+		EXAMPLES:
+		=========
+		>>> ModArrheniusCoef(2.0, -0.5, 3.0, 100.0).get_coef()
+    	0.19927962618542916
+		"""
+		if self.A < 0.0:
+			raise ValueError("A = {0:18.16e}:  Negative Arrhenius prefactor is prohibited!".format(self.A))
+
+		if self.T < 0.0:
+			raise ValueError("T = {0:18.16e}:  Negative temperatures are prohibited!".format(self.T))
+
+		if self.R < 0.0:
+			raise ValueError("R = {0:18.16e}:  Negative ideal gas constant is prohibited!".format(self.R))
+
+		if not np.isreal(self.b):
+			raise ValueError('Modified Arrhenius parameter b must be real!')
+
+		else:
+			try:
+				self.k = self.A * np.power(self.T, self.b) * np.exp(-self.E / (self.R * self.T))
+			except OverflowError:
+				raise OverflowError("The result is too large/small.")
+
+			return self.k
+
+		#doctest.testmod(extraglobs={'test_coef_const': ConstCoef(10.0)}, verbose=True)
--- test_reaction_coefs.py
+++ test_reaction_coefs.py
@@ -1,11 +1,72 @@
 import numpy as np
 from reaction_coefs import *
 
-def test_base_compute_coef():
+# tests for RxnCoef() base class
+def test_base_get_coef():
     try:
-        RxnCoef().compute_coef()
+        RxnCoef().get_coef()
     except NotImplementedError as err:
         assert(type(err) == NotImplementedError)
 
-def test_const_compute_coef():
-    assert ConstCoef(100).compute_coef() == 100
+
+# tests for ConstCoef() class
+def test_const_get_coef_result():
+    assert ConstCoef(100).get_coef() == 100
+
+def test_const_get_coef_neg_k():
+    try:
+        ConstCoef(-10).get_coef()
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+
+# tests for ArrheniusCoef() class
+def test_arr_get_coef_result():
+    assert ArrheniusCoef(A=np.power(10, 4), E=100, T=300, R=8.314).get_coef() == 9607.0007471344579
+
+def test_arr_get_coef_neg_T():
+    try:
+        ArrheniusCoef(A=np.power(10, 4), E=100, T=-300, R=8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+def test_arr_get_coef_neg_A():
+    try:
+        ArrheniusCoef(A=-np.power(10, 4), E=100, T=300, R=8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+def test_arr_get_coef_neg_R():
+    try:
+        ArrheniusCoef(A=np.power(10, 4), E=100, T=300, R=-8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+
+# tests for ArrheniusCoef() class
+def test_modarr_get_coef_result():
+    assert ModArrheniusCoef(A=np.power(10, 4), b=0.5, E=100, T=300, R=8.314).get_coef() == 166398.13402389048
+
+def test_modarr_get_coef_neg_T():
+    try:
+        ModArrheniusCoef(A=np.power(10, 4), b=0.5, E=100, T=-300, R=8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+def test_modarr_get_coef_neg_A():
+    try:
+        ModArrheniusCoef(A=-np.power(10, 4), b=0.5, E=100, T=300, R=8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+def test_modarr_get_coef_neg_R():
+    try:
+        ModArrheniusCoef(A=np.power(10, 4), b=0.5, E=100, T=300, R=-8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
+
+def test_modarr_get_coef_complex_b():
+    try:
+        ModArrheniusCoef(A=np.power(10, 4), b=(3+1j), E=100, T=300, R=8.314).get_coef()
+    except ValueError as err:
+        assert (type(err) == ValueError)
