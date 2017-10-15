0b89e2cb29cfe52cf527cc2caf2eb8dc41f1c53f
Michelle Ho <chiachi_ho@g.harvard.edu>
2017-10-15 12:21:29 -0400

    updated test_rxn.py file

--- test_rxns.py
+++ test_rxns.py
@@ -9,60 +9,89 @@ import rxns as rx
 
 
 # tests for Rxn() base class
-def test_base_progress_rate():
+def test_Rxn_progress_rate_not_implemented():
     try:
         rx.Rxn().progress_rate()
     except NotImplementedError as err:
         assert(type(err) == NotImplementedError)
 
-def test_base_reaction_rate():
+
+def test_Rxn_reaction_rate_not_implemented():
     try:
         rx.Rxn().reaction_rate()
     except NotImplementedError as err:
         assert(type(err) == NotImplementedError)
 
 
-# tests for IrrevElemRxn() class    
-def test_progress_rate_result_IrrevElem():
-    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+def test_Rxn_len_result():
+    reac1 = rx.Rxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]], [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+    assert len(reac1) == 2
+
+
+def test_Rxn_repr_result():
+    old_stdout = sys.stdout
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    reac1 = rx.Rxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]], [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+    print(reac1)
+    sys.stdout = old_stdout
+    assert capturedOutput.getvalue() == "Rxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]], [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]]\n"
+
+
+# tests for IrrevElemRxn() class
+def test_IrrevElem_len_result():
+    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]], [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+    assert len(reac1) == 2
+
+
+def test_IrrevElem_repr_result():
+    old_stdout = sys.stdout
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],
+                            [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+    print(reac1)
+    sys.stdout = old_stdout
+    assert capturedOutput.getvalue() == "IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]], [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]]\n"
+
+
+def test_IrrevElem_progress_rate_result():
+    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],
+                            [[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     assert (reac1.progress_rate() == [40.0, 10.0]).all()
 
-def test_len_function():
-    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
-    try:
-        reac1.__len__()
-    except TypeError as err:
-        assert(type(err) == TypeError)
-    
-def test_progress_rate_neg_ki_IrrevElem():
+
+def test_IrrevElem_progress_rate_neg_ki():
     reac1 = rx.IrrevElemRxn([-10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try:
         reac1.progress_rate()
     except ValueError as err:
         assert(type(err) == ValueError)
+
         
-def test_progress_rate_neg_xi_IrrevElem():
+def test_IrrevElem_progress_rate_neg_xi():
     reac1 = rx.IrrevElemRxn([10, 10], [-1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try: 
         reac1.progress_rate()
     except ValueError as err:
         assert(type(err) == ValueError)
 
-def test_progress_rate_dim_compat_IrrevElem():
+
+def test_IrrevElem_progress_rate_dim_compat():
     reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[ 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try:
         reac1.progress_rate()
     except ValueError as err:
         assert(type(err) == ValueError)
-
     
 
-def test_reaction_rate_result_IrrevElem():
+def test_IrrevElem_reaction_rate_result():
     reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
 
     assert (reac1.reaction_rate() == [-60., -70., 70.]).all()
 
-def test_reaction_rate_neg_ki_IrrevElem():
+
+def test_IrrevElem_reaction_rate_neg_ki():
     reac1 = rx.IrrevElemRxn([-10, 10], [1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try:
         reac1.reaction_rate()
@@ -70,7 +99,7 @@ def test_reaction_rate_neg_ki_IrrevElem():
         assert(type(err) == ValueError)
         
 
-def test_reaction_rate_neg_xi_IrrevElem():
+def test_IrrevElem_reaction_rate_neg_xi():
     reac1 = rx.IrrevElemRxn([10, 10], [-1.0, 2.0, 1.0], [[1.0, 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try:
          reac1.reaction_rate()
@@ -78,8 +107,8 @@ def test_reaction_rate_neg_xi_IrrevElem():
         assert(type(err) == ValueError)
 
 
-def test_reaction_rate_multi_dim_compat_IrrevElem():
-    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[ 2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+def test_IrrevElem_reaction_rate_dim_compat():
+    reac1 = rx.IrrevElemRxn([10, 10], [1.0, 2.0, 1.0], [[2.0, 0.0], [2.0, 0.0, 2.0]],[[0.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
     try:
         reac1.reaction_rate()
     except ValueError as err:
