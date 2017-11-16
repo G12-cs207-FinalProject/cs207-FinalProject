27e7b2ecfe81f06f3a8be08b524cbafa7f15eb80
Michelle Ho <chiachi_ho@g.harvard.edu>
2017-11-15 22:05:50 -0500

    documentation finished

--- driver.py
+++ driver.py
@@ -1,10 +1,19 @@
 from chemkin.preprocessing.parse_xml import XmlParser
 from chemkin.viz import summary
 
-Ti = [100, 750, 1500, 2500, 5000] 
-xi = [2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0] # specii concentration
+# Ti = [100, 750, 1500, 2500, 5000]
+# xi = [2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0] # specii concentration
+#
+# xml_file = './chemkin/xml-files/rxns_reversible.xml'
+# xml_parser = XmlParser(xml_file)
+# parsed_data_list = xml_parser.parsed_data_list(Ti)
+# summary.print_reaction_rate(parsed_data_list, xi)
 
-xml_file = './chemkin/xml-files/rxns_reversible.xml'
+
+Ti = [750, 1500, 2500]
+xi = [2.0, 1.0, 0.5, 1.0, 1.0] # specie concentrations
+
+xml_file = './chemkin/xml-files/rxns_hw5.xml'
 xml_parser = XmlParser(xml_file)
 parsed_data_list = xml_parser.parsed_data_list(Ti)
 summary.print_reaction_rate(parsed_data_list, xi)
