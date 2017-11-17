20ada5ade78138342e2a63a4b552687d06f8e746
Jiawen Tong <tongjw1@outlook.com>
2017-11-17 16:55:20 -0500

    add additional XML files for testing

--- driver.py
+++ driver.py
@@ -2,10 +2,14 @@ from chemkin import pckg_xml_path
 from chemkin.preprocessing.parse_xml import XmlParser
 from chemkin.viz import summary
 
+# Irreversible Reactions
+# ----------------------------
 # Ti = [750, 1500, 2500]
-# xi = [2.0, 1.0, 0.5, 1.0, 1.0]  # specie concentrations for 'rxns_hw5.xml'
+# xi = [2.0, 1.0, 0.5, 1.0, 1.0]  
 # xml_parser = XmlParser(pckg_xml_path('rxns_hw5'))
 
+# Reversible Reactions
+# ----------------------------
 Ti = [100, 750, 1500, 2500, 5000]
 xi = [2., 1., .5, 1., 1., 1., .5, 1.] # specie concentrations 'rxns_reversible.xml'
 xml_parser = XmlParser(pckg_xml_path('rxns_reversible'))
