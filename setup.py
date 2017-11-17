946744d6fe5a79c5c5d874a062dbc4e4e784f95f
Filip Michalsky <filipmichalsky@Filips-MacBook-Pro.local>
2017-11-16 21:40:55 -0500

    changed setup.py and added init to viz tests

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='chemkin',
-      version='2.1',
+      version='2.2',
       description='The best chemkin package',
       url='https://github.com/G12-cs207-FinalProject/cs207-FinalProject.git',
       author='Monsieur Package du Parser',
@@ -12,6 +12,9 @@ setup(name='chemkin',
                 'chemkin.preprocessing.tests',
                 'chemkin.reaction',
                 'chemkin.reaction.tests',
-                'chemkin.thermodynamics'],
+                'chemkin.thermodynamics',
+                'chemkin.thermodynamics.tests',
+                'chemkin.viz',
+                'chemkin.viz.tests'],
     package_data={'chemkin': ['*.sqlite',
                                 'xml-files/*.xml']})
