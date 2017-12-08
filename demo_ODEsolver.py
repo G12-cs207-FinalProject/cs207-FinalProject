9addfc1b3c525cb956403cac98f2b68a5950c3ee
Jiawen Tong <tongjw1@outlook.com>
2017-12-08 17:53:54 -0500

    changed ODE solver

--- demo_ODEsolver.py
+++ demo_ODEsolver.py
@@ -3,8 +3,7 @@ from chemkin.preprocessing.parse_xml import XmlParser
 from chemkin.viz import summary
 
 
-# Ti = [100, 900, 1500, 2500, 7000] # temp held constant
-Ti = [1500]
+Ti = [900, 2500]
 xi = [2., 1., .5, 1., 1., 1., .5, 1.] # specie concentrations
 xml_parser = XmlParser(pckg_xml_path('rxns_reversible'))
 
